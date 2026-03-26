d47e2c2975d037da7db152f8e4b4166a05d14710
수환 김 <leas019@suwon.ac.kr>
2026-03-26 22:42:12 +0900

    26.03.26

--- 111.cpp
+++ 111.cpp
@@ -2335,4 +2335,79 @@ int main() {
 
 }*/
 
+//LESSON12. 클래스의 기본================================================================
 
+/*
+//car 클래스의 선언
+
+class Car {
+public:
+	int num;
+	double gas;
+	void show();
+};
+
+//car 클래스 멤버 함수의 정의
+void Car::show()
+{
+	cout << "차량 번호는" << num << "입니다.\n";
+	cout << "연료량은" << gas << "입니다.\n";
+}
+
+int main() {
+	Car car1;
+
+	car1.num = 1234;
+	car1.gas = 20.5;
+
+	car1.show();
+
+	return 0;
+}*/
+
+//LESSON 12.2. 멤버에 대한 접근 제한============================================
+
+//Car class 선언
+class Car {
+private: //private 선언
+	int num;
+	double gas;
+public: // 멤버 함수는 public으로 만듬
+	void show();
+	void setNumGas(int n, double g);
+};
+
+//Car class 멤버 함수의 정의
+void Car::show()
+{
+	cout << "차량 번호는" << num << "입니다.\n";
+	cout << "연료량은" << gas << "입니다.\n";
+}
+void Car::setNumGas(int n, double g)
+{
+	if (g > 0 && g < 1000) {
+		num = n;
+		gas = g;
+		cout << "차량 번호를" << num << "으로 연료량을" << gas << "으로 바꾸었습니다.\n";
+	}
+	else {
+		cout << g << "는 올바른 연료량이 아닙니다.\n";
+		cout << "연료량을 바꿀 수 없습니다.\n";
+	}
+}
+int main() {
+	Car car1;
+
+	//이러한 접근이 불가능하게 됩니다.
+	//car1.num =1234;
+	//car1.gas = 20.5;
+
+	car1.setNumGas(1234, 20.5);
+	car1.show();
+
+	cout << "잘못된 연료랼(-10.0)을 저장해 보겠습니다.....\n";
+	car1.setNumGas(1234, -10.0);
+	car1.show();
+
+	return 0;
+}
